84b247d91dc522fb72e57960d7f06ad825165446
iannathan-k <ianmtv@gmail.com>
2025-01-01 23:35:40 +0700

    created evaluator

---
+++ evaluator.py
@@ -0,0 +1,51 @@
+from pieces import *
+from math import inf
+
+def determine_winner(board, turn):
+    # turn = True --> White
+    # turn = False --> Black
+    white_move = False
+    black_move = False
+    for i in range(9):
+        piece = board[i // 3][i % 3]
+
+        if piece.get_color() == PieceColor.WHITE:
+            if piece.get_position() in range(6, 8):
+                return PieceColor.WHITE
+            if piece.get_moves(board):
+                white_move = True
+
+        elif piece.get_color() == PieceColor.BLACK:
+            if piece.get_position() in range(0, 2):
+                return PieceColor.BLACK
+            if piece.get_moves(board):
+                black_move = True
+
+    if white_move == False and turn:
+        return PieceColor.BLACK
+    elif black_move == False and not turn:
+        return PieceColor.WHITE
+
+    return PieceColor.EMPTY
+
+def evaluate_board(board, turn):
+    if determine_winner(board, turn) == PieceColor.BLACK:
+        return -inf
+    elif determine_winner(board, turn) == PieceColor.WHITE:
+        return inf
+
+    white_advantage = 0
+    black_advantage = 0
+
+    for i in range(9):
+        piece = board[i // 3][i % 3]
+        if piece.get_color() == PieceColor.WHITE:
+            white_advantage += 1 # piece count advantage
+            white_advantage += (i // 3) * 2 # distance advantage
+            white_advantage += len(piece.get_moves(board)) # freedom advantage
+        elif board[i // 3][i % 3].get_color() == PieceColor.BLACK:
+            black_advantage += 1
+            black_advantage += (2 - i // 3) * 2
+            black_advantage += len(piece.get_moves(board))
+
+    return white_advantage - black_advantage
--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from pieces import *
+from evaluator import *
 
-board = [[None for i in range(8)] for j in range(8)]
+board = [
+    [Empty(), Empty(), Empty()],
+    [Empty(), Empty(), Empty()],
+    [Empty(), Empty(), Empty()]
+]
 
-for i in range(64):
-    board[i // 8][i % 8] = Empty(i)
+for i in range(3):
+    board[i // 3][i % 3] = Pawn(i, PieceColor.WHITE, +1)
 
-board[3][7] = Knight(31, PieceColor.WHITE)
+for i in range(6,9):
+    board[i // 3][i % 3] = Pawn(i, PieceColor.BLACK, -1)
 
-print(board[3][7].find_moves(board))
+def print_board():
+    print("+-+-+-+")
+    for line in board:
+        line_string = "|"
+        for piece in line:
+            if piece.get_color() == PieceColor.EMPTY:
+                line_string += " |"
+            elif piece.get_color() == PieceColor.WHITE:
+                line_string += "P|"
+            else:
+                line_string += "p|"
+        print(line_string)
+        print("+-+-+-+")
+
+def move_piece(origin_pos, target_pos):
+    board[target_pos // 3][target_pos % 3] = board[origin_pos // 3][origin_pos % 3]
+    board[origin_pos // 3][origin_pos % 3] = Empty()
+    board[target_pos // 3][target_pos % 3].set_position(target_pos)
